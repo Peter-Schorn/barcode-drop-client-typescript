16745bc685fa4e38e5e97737f3be6a6b92809ad8
Peter Schorn <petervschorn@gmail.com>
2025-01-12 17:14:58 -0600

    Convert DebugBreakpointView to a typescript functional component.

--- src/Components/DebugBreakpointView.tsx
+++ src/Components/DebugBreakpointView.tsx
@@ -1,21 +1,19 @@
-import { Component } from "react";
+import { type JSX } from "react";
 
-export class DebugBreakpointView extends Component {
+export function DebugBreakpointView(): JSX.Element {
 
-    render() {
-        return (
-            <div 
-                className="position-fixed text-nowrap rounded-3 p-1 m-3 bg-info opacity-50 top-0 end-0" 
-                style={{ zIndex: "100" }}
-            >
-                <div className="d-block d-sm-none">xs</div>
-                <div className="d-none d-sm-block d-md-none">sm</div>
-                <div className="d-none d-md-block d-lg-none">md</div>
-                <div className="d-none d-lg-block d-xl-none">lg</div>
-                <div className="d-none d-xl-block d-xxl-none">xl</div>
-                <div className="d-none d-xxl-block">xxl</div>
-            </div>
-        );
-    };
+    return (
+        <div
+            className="position-fixed text-nowrap rounded-3 p-1 m-3 bg-info opacity-50 top-0 end-0"
+            style={{ zIndex: "100" }}
+        >
+            <div className="d-block d-sm-none">xs</div>
+            <div className="d-none d-sm-block d-md-none">sm</div>
+            <div className="d-none d-md-block d-lg-none">md</div>
+            <div className="d-none d-lg-block d-xl-none">lg</div>
+            <div className="d-none d-xl-block d-xxl-none">xl</div>
+            <div className="d-none d-xxl-block">xxl</div>
+        </div>
+    );
 
 }
